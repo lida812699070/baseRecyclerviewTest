f5d0c198ec28bd1a43db0e1134666186e38ac55e
李达 <81269907@qq.com>
2017-12-13 20:28:57 +0800

    Initial commit

---
+++ app/src/main/java/commercialvehicle/careagle/com/baserecyclerviewtest/MainActivity.java
@@ -0,0 +1,27 @@
+package commercialvehicle.careagle.com.baserecyclerviewtest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by lida on 2017/12/13.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbarTb;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbarTb = (Toolbar) findViewById(R.id.tb_toolbar);
+        if (mToolbarTb !=null) {
+            setSupportActionBar(mToolbarTb);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
